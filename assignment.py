acdbb6e8ef2a0ed8156009b9c7b3b5dc63090192
Sichun Wu <s.wu6@students.uu.nl>
2023-02-22 00:27:58 +0100

    get config.xml

--- assignment.py
+++ assignment.py
@@ -5,7 +5,8 @@ import cv2
 import p1
 
 block_size = 1.0
-
+w = 8
+h = 6
 
 def generate_grid(width, depth):
     # Generates the floor grid locations
@@ -50,10 +51,10 @@ def get_cam_rotation_matrices():
     return cam_rotations
 
 # capture images from intrinsics.avi
-def Capture(Cam):
+def capture(Cam):
     cap = cv2.VideoCapture('./data/cam{}/intrinsics.avi'.format(Cam))
     totalFrames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    index = random.sample(range(totalFrames), 25)
+    index = random.sample(range(totalFrames), 50)
     frameIndex = 0
     while True:
         ret, frame = cap.read()
@@ -66,8 +67,96 @@ def Capture(Cam):
     cap.release()
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+# get the extrinsic parameters
+def getExtrinsics(Cam):
+    # get the image points (only run once)
+    # cap = cv2.VideoCapture('./data/cam{}/checkerboard.avi'.format(Cam))
+    # ret, frame = cap.read()
+    # filename = f'./data/cam{Cam}/capture.jpg'
+    # cv2.imwrite(filename, frame)
+    # p1.firstRun(filename)
+
+    # load the image points and intrinsics
+    fs = cv2.FileStorage('./data/cam{}/imageCorners.xml'.format(Cam), cv2.FILE_STORAGE_READ)
+    data = fs.getNode('corners').mat()
+    imageCorners = np.array(data)
+    fs = cv2.FileStorage('./data/cam{}/intrinsics.xml'.format(Cam), cv2.FILE_STORAGE_READ)
+    mtx = fs.getNode('mtx').mat()
+    dist = fs.getNode('dist').mat()
+
+    objp = np.zeros((w * h, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:w, 0:h].T.reshape(-1, 2) * 115
+
+    # get the extrinsic parameters
+    retval, rvec, tvec = cv2.solvePnP(objectPoints=objp, imagePoints=imageCorners, cameraMatrix=mtx,
+                                      distCoeffs=dist)
+    R, _ = cv2.Rodrigues(rvec)  # change rotation vector to matrix
+    T, _ = cv2.Rodrigues(tvec)  # change translation vector to matrix
+
+    fs = cv2.FileStorage('./data/cam{}/config.xml'.format(Cam), cv2.FILE_STORAGE_WRITE)
+    fs.write("mtx", mtx)
+    fs.write("dist", dist)
+    fs.write("rvec", rvec)
+    fs.write("tvec", tvec)
+
+    # draw the X, Y, Z axes on the image
+    img = cv2.imread('./data/cam{}/capture.jpg'.format(Cam))
+    worldPoints = np.float32([[0, 0, 0], [300, 0, 0], [0, 300, 0], [0, 0, 300]])
+    image_pts, _ = cv2.projectPoints(worldPoints, rvec, tvec, mtx, dist)
+
+    image_pts = np.int32(image_pts).reshape(-1, 2)
+    img = cv2.line(img, tuple(image_pts[0]), tuple(image_pts[1]), (0, 0, 255), 1)  # X axis
+    img = cv2.line(img, tuple(image_pts[0]), tuple(image_pts[2]), (0, 255, 0), 1)  # Y axis
+    img = cv2.line(img, tuple(image_pts[0]), tuple(image_pts[3]), (255, 0, 0), 1)  # Z axis
+    cv2.imwrite('./data/cam{}/captureXYZ.jpg'.format(Cam), img)
+
+    cv2.imshow('capture', img)
+    cv2.waitKey(1000)
+    cv2.destroyAllWindows()
+
+
+# find corners automatically, this does not work, can only get the points of the paper.
+def findCorners(Cam):
+    cap = cv2.VideoCapture('./data/cam{}/checkerboard.avi'.format(Cam))
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 155, 255, cv2.THRESH_BINARY)
+
+    # find the polygon contours
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    approx = cv2.approxPolyDP(contours[0], 0.01 * cv2.arcLength(contours[0], True), True)
+    # cv2.drawContours(frame, [approx], 0, (128, 0, 0), 1)
+
+    # get the corner points
+    cornerPoints = approx.reshape(-1, 2)
+    # print(corner_points)
+    for point in cornerPoints:
+        cv2.circle(frame, tuple(point), 1, (0, 0, 255), -1)
+
+    # set the pixels outside the polygon to green
+    # mask = np.zeros(frame.shape[:2], np.uint8)
+    # cv2.drawContours(mask, [approx], 0, 255, -1)
+    # result = np.full(frame.shape, (0, 128, 0), dtype=np.uint8)
+    # result[mask != 0] = frame[mask != 0]
+    # cv2.imshow('image', result)
+    # filename = f'./data/cam{Cam}/extractChessboard.jpg'
+    # cv2.imwrite(filename, result)
+
+    cv2.imshow('image', frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return cornerPoints
+
+# get camera parameters (only run once)
+def getCameraParam():
     for i in range(4):
-        # Capture(i+1)
+        capture(i+1)            # capture images
         filename = f'./data/cam{i+1}/capture/*.jpg'
-        p1.firstRun(filename)
+        p1.run(25, filename)    # get intrinsics
+        findCorners(i+1)        # find corners automatically (doesn't work)
+        getExtrinsics(i+1)      # get extrinsics
+
+if __name__ == '__main__':
+    print('1')
+
--- p1.py
+++ p1.py
@@ -3,7 +3,6 @@ import numpy as np
 import glob
 import win32api,win32con
 import time
-import xml.etree.ElementTree as ET
 
 w = 8
 h = 6
@@ -31,6 +30,8 @@ subCoord = []
 # image number for saving the graph
 imgNum = 1
 
+fName = []
+
 # Display the coordinates of the points clicked on the image and find all points
 def click_event(event, x, y, flags, params):
     global coordinates, subCoord, clickNum
@@ -40,7 +41,8 @@ def click_event(event, x, y, flags, params):
         font = cv2.FONT_HERSHEY_SIMPLEX
         # displaying the coordinates on the Shell
         if clickNum < 4:
-            cv2.putText(params, str(x) + ',' + str(y), (x, y), font, 1, (255, 0, 0), 2)
+            #cv2.putText(params, str(x) + ',' + str(y), (x, y), font, 1, (255, 0, 0), 2)
+            cv2.circle(params, (x, y), 1, (128, 0, 0), -1)
             cv2.imshow('findCorners', params)
             coordinates.append([x, y])
         clickNum += 1
@@ -56,9 +58,9 @@ def click_event(event, x, y, flags, params):
             b = params[y, x, 0]
             g = params[y, x, 1]
             r = params[y, x, 2]
-            cv2.putText(params, str(b) + ',' + str(g) + ',' + str(r), (x, y), font, 1, (255, 255, 0), 2)
+            #cv2.putText(params, str(b) + ',' + str(g) + ',' + str(r), (x, y), font, 1, (255, 255, 0), 2)
+            cv2.circle(params, (x, y), 1, (0, 255, 0), -1)
             cv2.imshow('findCorners', params)
-            cv2.circle(params, (x, y), 5, (0, 255, 0), -1)
             coordinates.append([x, y])
         clickNum += 1
         if clickNum == 4:
@@ -66,7 +68,7 @@ def click_event(event, x, y, flags, params):
 
 # Calculate the transformation matrix and use it to find all points
 def subcoordinates(img):
-    global coordinates, subCoord, clickNum, imgNum
+    global coordinates, subCoord, clickNum, imgNum, fName
 
     # compute all coordinates
     worldCoord = np.array([[0,(h-1)*115], [(w-1)*115,(h-1)*115], [(w-1)*115,0], [0,0]], np.float32)
@@ -74,20 +76,30 @@ def subcoordinates(img):
     M = cv2.getPerspectiveTransform(worldCoord, coordinates_array)
     res = cv2.perspectiveTransform(subWorkCoord.reshape(-1, 1, 2), M)
 
-    # show the chessboard grid
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # save the corners into a file
     corners = np.array(res, np.float32)
-    corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-    cv2.drawChessboardCorners(img, (w, h), corners2, True)
+    # print(corners)
+    newfile = fName.replace('capture.jpg', 'imageCorners.xml')
+    fs = cv2.FileStorage(newfile, cv2.FILE_STORAGE_WRITE)
+    fs.write("corners", corners)
+
+    # show the chessboard grid
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+    # cv2.drawChessboardCorners(img, (w, h), corners, True)
+    corners = np.int32(corners)
+    corner_points = corners.reshape(-1, 2)
+    for point in corner_points:
+        cv2.circle(img, tuple(point), 1, (0, 0, 255), -1)
     cv2.imshow('findCorners', img)
 
     # save the corners
     objpoints.append(objp)
-    imgpoints.append(np.array(corners2, np.float32))
+    imgpoints.append(np.array(corners, np.float32))
 
     # save the image
-    cv2.imwrite("./ChessboardCornersImg/Run1/image{}.jpg".format(imgNum), img)
-    imgNum += 1
+    # cv2.imwrite("./ChessboardCornersImg/Run1/image{}.jpg".format(imgNum), img)
+    # imgNum += 1
 
     #clear the numbers
     coordinates = []
@@ -96,7 +108,8 @@ def subcoordinates(img):
 
 # first run
 def firstRun(filename):
-    global imgNum
+    global imgNum, fName
+    fName = filename
     images = glob.glob(filename)
     i = 0
     for fname in images:
@@ -120,8 +133,8 @@ def firstRun(filename):
             imgNum += 1
         else:
             if i == 0:
-                win32api.MessageBox(0, "If detect corners fail, please choose 4 corners clockwise, "
-                                       "starting from the top-left.","Notice", win32con.MB_OK)
+                # win32api.MessageBox(0, "If detect corners fail, please choose 4 corners clockwise, "
+                #                       "starting from the top-left.","Notice", win32con.MB_OK)
                 i += 1
             cv2.namedWindow('findCorners', cv2.WINDOW_NORMAL)
             cv2.resizeWindow('findCorners', 640, 480)
@@ -129,43 +142,22 @@ def firstRun(filename):
             cv2.setMouseCallback('findCorners', click_event, img)
             cv2.waitKey(0)
         cv2.destroyAllWindows()
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    # ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
     # np.save('./CameraParams/Run1/mtx.npy', mtx)
     # np.save('./CameraParams/Run1/dist.npy', dist)
     # np.save('./CameraParams/Run1/rvecs.npy', rvecs)
     # np.save('./CameraParams/Run1/tvecs.npy', tvecs)
 
-    root = ET.Element('Config')
-    mtx_elem = ET.SubElement(root, 'mtx')
-    dist_elem = ET.SubElement(root, 'dist')
-    rvecs_elem = ET.SubElement(root, 'rvecs')
-    tvecs_elem = ET.SubElement(root, 'tvecs')
-    mtx_elem.text = np.array2string(mtx)
-    dist_elem.text = np.array2string(dist)
-    rvecs_str = "["
-    for r in rvecs:
-        rvecs_str += np.array2string(r, separator=" ") + " "
-    rvecs_elem.text = rvecs_str[:-1] + "]"
-    tvecs_str = "["
-    for t in tvecs:
-        tvecs_str += np.array2string(t, separator=" ") + " "
-    tvecs_elem.text = tvecs_str[:-1] + "]"
-
-    xml_data = ET.tostring(root, encoding='utf-8', method='xml')
-    newfile = filename.replace('capture/*.jpg', 'config.xml')
-    with open(newfile, 'wb') as f:
-        f.write(xml_data)
-
     # reject low quality results
     # rejection(rvecs, tvecs, mtx, dist, gray, 1)
 
 # second and third run
-def run(round):
+def run(round, filename):
     global objpoints, imgpoints
     objpoints = []  # 3d point in real world space
     imgpoints = []  # 2d points in image plane
 
-    images = glob.glob('./CameraRoll/*.jpg')
+    images = glob.glob(filename)
     i = 0
     for fname in images:
         if i < round:
@@ -181,27 +173,38 @@ def run(round):
                 imgpoints.append(corners2)
                 # Draw and save the images
                 cv2.drawChessboardCorners(img, (w, h), corners2, ret)
-                if round == 10:
-                    cv2.imwrite("./ChessboardCornersImg/Run2/image{}.jpg".format(i+1), img)
-                if round == 5:
-                    cv2.imwrite("./ChessboardCornersImg/Run3/image{}.jpg".format(i+1), img)
+                # if round == 10:
+                #     cv2.imwrite("./ChessboardCornersImg/Run2/image{}.jpg".format(i+1), img)
+                # if round == 5:
+                #     cv2.imwrite("./ChessboardCornersImg/Run3/image{}.jpg".format(i+1), img)
+
+                print(fname)
+                # cv2.namedWindow('findCorners', cv2.WINDOW_NORMAL)
+                # cv2.resizeWindow('findCorners', 640, 480)
+                # cv2.imshow('findCorners', img)
+                # cv2.waitKey(1000)
                 i += 1
         else:
             break
         cv2.destroyAllWindows()
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    runNum = 0
-    if round == 10:
-        runNum = 2
-    if round == 5:
-        runNum = 3
-    np.save('./CameraParams/Run{}/mtx.npy'.format(runNum), mtx)
-    np.save('./CameraParams/Run{}/dist.npy'.format(runNum), dist)
-    np.save('./CameraParams/Run{}/rvecs.npy'.format(runNum), rvecs)
-    np.save('./CameraParams/Run{}/tvecs.npy'.format(runNum), tvecs)
+    print(filename, i)
+    # runNum = 0
+    # if round == 10:
+    #     runNum = 2
+    # if round == 5:
+    #     runNum = 3
+    # np.save('./CameraParams/Run{}/mtx.npy'.format(runNum), mtx)
+    # np.save('./CameraParams/Run{}/dist.npy'.format(runNum), dist)
+    # np.save('./CameraParams/Run{}/rvecs.npy'.format(runNum), rvecs)
+    # np.save('./CameraParams/Run{}/tvecs.npy'.format(runNum), tvecs)
 
+    newfile = filename.replace('capture/*.jpg', 'intrinsics.xml')
+    fs = cv2.FileStorage(newfile, cv2.FILE_STORAGE_WRITE)
+    fs.write("mtx", mtx)
+    fs.write("dist", dist)
     # reject low quality results
-    rejection(rvecs, tvecs, mtx, dist, gray, runNum)
+    # rejection(rvecs, tvecs, mtx, dist, gray, runNum)
 
 # draw XYZ axis
 def draw_axis(img, corner, imgpts):
